0c323e5606ed5d8dd8d0d2c3bb7edc9355a5b294
Jeremiah Anim <jjanim524@gmail.com>
2023-03-14 21:58:38 -0400

    work lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import APIform from './components/APIform';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentImage,setCurrentImage] = useState(null); 
+
+  const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+
+
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    }else{
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == ""){
+          inputs[key] = defaultValues[key]
+      }
+    }
+    }
+    makeQuery()
+  }
+
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;    
+
+  }
+  
+
+  const callAPI = async (query) => {
+    
+    const response = await fetch(query);
+    const json = await response.json();
+  
+}
+    
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <div className="whole-page">
+    <h1>Build Your Own Screenshot! 📸</h1>
+    
+    <APIform
+      inputs={inputs}
+      handleChange={(e) =>
+        setInputs((prevState) => ({
+          ...prevState,
+          [e.target.name]: e.target.value.trim(),
+        }))
+      }
+      onSubmit={submitForm}
+    />
+    <br></br>
+
+  </div>
+  );
 }
 
 export default App
